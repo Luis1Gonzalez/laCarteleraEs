102d6ad849b4c013ce64f0667ef24e82c13e2c7e
luis1gonzalez <luis1gonzalez@hotmail.com>
2022-07-29 18:57:30 +0200

    agregando cosas al home

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -12,11 +12,11 @@ import Form from "../../components/form";
 export default function User(){
   const [t, i18n] = useTranslation("global");
   const navigate = useNavigate();
-  const [film, setFilm] = useState({});
+  const [films, setFilms] = useState({});
   let [title,setTitle] = useState("")
   
 
-  // let urlOriginal = `http://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_API_KEY_OMDB}&type=movie`
+  
   let tittle=""
 if (title === ""){
   tittle='hulk'
@@ -28,11 +28,20 @@ const getTitle = async (title) => {
   setTitle(title)
 }    
 
+
+
     useEffect(() =>{ 
-  fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${process.env.REACT_APP_API_KEY_OMDB}&type=movie&s=${tittle}`)
+  fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_kEY_TMDB_ONLY}&language=es-ES&query=${tittle}`)
   .then((r =>r.json()))
-  .then(f => setFilm(f))
+  .then(f => setFilms(f))
     },[tittle])
+
+
+
+console.log(tittle)
+
+
+    
 
   return(
 
@@ -45,15 +54,15 @@ const getTitle = async (title) => {
          
 
          <div className="card-search">
-          {film.Search?.map((f) => (
-            <div className="film-card" key={f.imdbID} onClick = {() => navigate(`/film/${f.Title}`)} >
-              <h1>{f.Title}</h1>
+          {films?.results?.map((f) => (
+            <div className="film-card" key={f.id} onClick = {() => navigate(`/film/${f.id}`)} >
+              <h3>{f.title}</h3>
               
               <div>              
-                <img src={f.Poster ? f.Poster : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.dCO_yi0ryq5_JUv7fMtqQQHaEK%26pid%3DApi&f=1' } alt={`poster de ${f.Title}`} />
+                <img src={f.poster_path ? `https://image.tmdb.org/t/p/w500/${f.poster_path}` : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.dCO_yi0ryq5_JUv7fMtqQQHaEK%26pid%3DApi&f=1' } alt={`poster de ${f.title}`} />
               </div>
-              <h2>{f.Title}</h2>
-              <h3>{f.Year}</h3>
+              <h4>{f.title}</h4>
+              <h5>{f.release_date}</h5>
             </div>
           )
           )
--- src/pages/userDetail/index.jsx
+++ src/pages/userDetail/index.jsx
@@ -12,37 +12,42 @@ const UserMovieDetail = () => {
   // const token =sessionStorage.getItem('auth_token')
   const [t, i18n] = useTranslation("global");
   const { tittle } = useParams();
-  const [film, setFilm] = useState({});
+  const [movie, setMovie] = useState({});
 
+  // https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_kEY_TMDB_ONLY}&language=es-ES&query=${tittle}
+  // http://api.themoviedb.org/3/movie/${movie_id}?api_key=279a6209c3a88953123109103b1416fd
 
   useEffect(() => {
-    fetch(`https://www.omdbapi.com/?plot=full&apikey=${process.env.REACT_APP_API_KEY_OMDB}&t=${tittle}`)
-    // fetch(`https://api.themoviedb.org/3/search/movie?api_key=279a6209c3a88953123109103b1416fd&language=es-ES&query=${tittle}`)
+    fetch(`http://api.themoviedb.org/3/movie/${tittle}?api_key=279a6209c3a88953123109103b1416fd&language=es-ES`)
 
       .then((r) => r.json())
-      .then((f) => setFilm(f));
+      .then((f) => setMovie(f));
   }, [tittle]);
 
-
+console.log(movie)
   return (
     <div className="wrap-details">
         <div className="wrap-film">
 
       <div className="wrap-poster">
-        <img src={film.Poster ? film.Poster : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.dCO_yi0ryq5_JUv7fMtqQQHaEK%26pid%3DApi&f=1'} alt={`poster de ${film.Title}`} />
+        <img src={movie.backdrop_path ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}` : 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.dCO_yi0ryq5_JUv7fMtqQQHaEK%26pid%3DApi&f=1'} alt={`poster de ${movie.title}`} />
       </div>
+
+     
       <div className="wrap-description">
-        <h1>{film.Title}</h1>
-        <p>{`${t('details.genre')}: ${film.Genre}`}</p>
-        <p>{`${t('details.year')}: ${film.Year}`}</p>
-        <p>{`${t('details.country')}: ${film.Country}`}</p>
+        <h1>{movie.original_title}</h1>
+        {/* <p>{`${t('details.genre')}: ${film.Genre}`}</p> */}
+        <p>{`${t('details.year')}: ${movie.release_date}`}</p>
+        {/* <p>{`${t('details.country')}: ${film.Country}`}</p>
         <p>{`Director: ${film.Director}`}</p>
         <p>{`${t('details.writer')}: ${film.Writer}`}</p>
         <p>{`${t('details.actors')}: ${film.Actors}`}</p>
         <p>{`${t('details.budget')}: ${film.BoxOffice}`}</p>        
-        <p>{`${t('details.time')}: ${film.Runtime}`}</p>   
-        <p>{`${t('details.awards')}: ${film.Awards}`}</p>    
+        <p>{`${t('details.time')}: ${film.Runtime}`}</p>   */}
+        <p>{`${t('details.awards')}: ${movie.overview}`}</p>     
       </div>
+
+
       </div>
     </div>
   );
